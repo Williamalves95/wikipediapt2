dfc062668a5f2c6421c22f0395c72d897520af6c
william <williamalves95@hotmail.com>
2021-03-17 20:41:45 -0300

    Ajuste da posição da chave no arquivo gradle.buil e atualização do seletor do botao Buscar no script do pacote Simples

--- src/test/java/steps/Post.java
+++ src/test/java/steps/Post.java
@@ -1,5 +1,8 @@
 package steps;
 
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
 import cucumber.api.java.pt.Dado;
 import cucumber.api.java.pt.Entao;
 import cucumber.api.java.pt.Quando;
@@ -8,22 +11,42 @@ import org.openqa.selenium.Keys;
 
 import static org.junit.Assert.assertTrue;
 
-public class Post extends Base{
+public class Post extends Base {
     private Base base;
 
-    public Post(Base base) { this.base = base; }
+    public Post(Base base) {
+        this.base = base;
+    }
 
-    @Dado("^que acesso a Wikipeida em Portugues$")
-    public void queAcessoAWikipeidaEmPortugues() {
+    /*
+
+        @Dado("^que acesso a Wikipeida em Portugues$")
+        public void queAcessoAWikipeidaEmPortugues() {
+            base.driver.get(base.url);
+        }
+
+        @Quando("^pesquiso por \"([^\"]*)\"$")
+        public void pesquisoPor(String produto)  {
+            base.driver.findElement(By.id("searchInput")).sendKeys(produto + Keys.ENTER);}
+
+        @Entao("^Exibe a expressao \"([^\"]*)\" no titulo da guia$")
+        public void exibeAExpressaoNoTituloDaGuia(String produto) {
+            assertTrue(base.driver.getTitle().contains(produto));
+        }
+        */
+    @Given("^que acesso a Wikipedia em Portugues$")
+    public void queAcessoAWikipediaEmPortugues() {
         base.driver.get(base.url);
     }
 
-    @Quando("^pesquiso por \"([^\"]*)\"$")
-    public void pesquisoPor(String produto)  {
-        base.driver.findElement(By.id("searchInput")).sendKeys(produto + Keys.ENTER);}
+    @When("^pesquiso por \"([^\"]*)\"$")
+    public void pesquisoPor(String produto) {
+        base.driver.findElement(By.id("searchInput")).sendKeys(produto + Keys.ENTER);
+    }
 
-    @Entao("^Exibe a expressao \"([^\"]*)\" no titulo da guia$")
+    @Then("^Exibe a expressao \"([^\"]*)\" no titulo da guia$")
     public void exibeAExpressaoNoTituloDaGuia(String produto) {
         assertTrue(base.driver.getTitle().contains(produto));
     }
+
 }
